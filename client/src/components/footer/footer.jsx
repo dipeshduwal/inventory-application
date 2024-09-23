c4fad1d6640086ee52764de0b29bc5c358b96669
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-23 06:50:47 +0545

    Create footer

---
+++ client/src/components/footer/footer.jsx
@@ -0,0 +1,15 @@
+import './footer.css';
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="credit">Developed By Bahadır ÜNGÖR</div>
+      <a href="https://linkedin.com/in/bahaungor" target="blank" rel="noreferrer noopener">
+        <img src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg" alt="bahaungor" />
+      </a>
+      <a href="https://github.com/bahaungor" target="_blank" rel="noreferrer noopener">
+        <img src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/github.svg" alt="bahaungor" />
+      </a>
+    </footer>
+  );
+}
